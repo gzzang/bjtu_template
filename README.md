d84444db061d442d4fce08888f4df5e0c09a4c92
gzzang <gzzang@qq.com>
2021-04-15 11:04:44 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# 北京交通大学博士学位论文模板
+
+作者： [@gzzang](https://github.com/gzzang)
+
+## 说明
+
+本模板仅适用于双面打印的北京交通大学博士学位论文，使用 XeTeX 引擎进行编译。
+* 注意：非官方，且不适用于北京交通大学硕士学位论文
+
+## 编译
+
+可直接运行 compile.bat 文件。
+
+## 致谢
+
+* 感谢制作初始模板的北交大同学；
+* 感谢[@minghua.li](https://github.com/learup和)[@LiangJinpeng](https://github.com/LiangJinpeng)对模板的巨大贡献。
+
+## 软件许可证
+
+使用 [MIT](LICENSE) 授权。
---
+++ compile.bat
@@ -0,0 +1,25 @@
+@echo off
+del bjtu_template.pdf >nul 2>nul
+if exist bjtuthesis.pdf (
+	echo Close the file: bjtu_template.pdf!!!
+	echo Or use Sumatra to open bjtu_template.pdf
+	pause
+	exit
+)
+
+
+echo Compile...
+echo clean files...
+del *.aux *.run.xml *.bcf *.log *.xdv *.bbl *.bak *.blg *.out *.thm *.toc *.synctex* *.glg *.glo *.gls *.ist *.idx *.ilg *.ind *.acn *.acr *.lof *.lot *.alg *.glsdefs >nul 2>nul
+echo If stuck, look for the error in texstudio
+echo xelatex -no-pdf bjtu_template...
+xelatex -no-pdf bjtu_template >nul
+echo bibtex --debug bjtu_template...
+bibtex bjtu_template >nul
+echo xelatex bjtu_template twice...
+xelatex bjtu_template >nul
+xelatex bjtu_template >nul
+echo clean files...
+del *.aux *.run.xml *.bcf *.log *.xdv *.bbl *.bak *.blg *.out *.thm *.toc *.synctex* *.glg *.glo *.gls *.ist *.idx *.ilg *.ind *.acn *.acr *.lof *.lot *.alg *.glsdefs >nul 2>nul
+echo open file
+bjtu_template.pdf
